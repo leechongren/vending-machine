b659d7f3cd8419a410b06dc37c5aa3bfb4273e5c
leechongren <lee_chongren@hotmail.com>
2020-03-25 00:57:42 +0800

    test github

--- CoconutVendingMachine.js
+++ CoconutVendingMachine.js
@@ -4,7 +4,13 @@ class CoconutVendingMachine extends VendingMachine {
   constructor() {
     super();
     this.coconutWaterPrice = 350;
+    this.menu = {
+      OJ: this.orangeJuicePrice,
+      CW: this.coconutWaterPrice
+    };
   }
+
+  checkChoice(choice) {}
 }
 
 module.exports = CoconutVendingMachine;
